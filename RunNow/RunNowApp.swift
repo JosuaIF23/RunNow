727aa83a78c1150d66d544d5434522ef4f2867f3
JosuaIF23 <firenoobgaming@gmail.com>
2025-06-18 17:46:52 +0700

    Initial Commit

---
+++ RunNow/RunNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunNowApp.swift
+//  RunNow
+//
+//  Created by Foundation-010 on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RunNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunNowTests/RunNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunNowTests.swift
+//  RunNowTests
+//
+//  Created by Foundation-010 on 18/06/25.
+//
+
+import Testing
+@testable import RunNow
+
+struct RunNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
