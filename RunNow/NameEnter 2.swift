39d3dbe8964a06e05077176a6e2d4bc8b99c5aec
JosuaIF23 <firenoobgaming@gmail.com>
2025-06-24 15:37:29 +0700

    commnit 1 santo

---
+++ RunNow/NameEnter 2.swift
@@ -0,0 +1,54 @@
+//
+//  NameEnter.swift
+//  RunNow
+//
+//  Created by Foundation-010 on 19/06/25.
+//
+
+import SwiftUI
+
+struct NameEnter: View {
+    @State private var name: String = ""
+    @State private var goToNext = false
+
+    var body: some View {
+        VStack {
+            Spacer()
+
+            VStack(spacing: 20) {
+                Text("Burn Cal App")
+                    .font(.largeTitle)
+                    .bold()
+
+                TextField("Enter your name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal)
+                    .frame(maxWidth: 300)
+
+                NavigationLink(destination: BMIInputView(name: name), isActive: $goToNext) {
+                    EmptyView()
+                }
+
+                Button("Save") {
+                    if !name.isEmpty {
+                        goToNext = true
+                    }
+                }
+                .bold()
+                .padding(.vertical, 10)
+                .padding(.horizontal, 40)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            }
+
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+
+#Preview {
+    NameEnter()
+}
